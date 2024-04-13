df25bf11fc7ef292c785d905183f968b18acd5c4
Blayze Millward <blayze.millward@googlemail.com>
2024-04-13 18:08:17 +0100

    Initial commit with basic websocket server.

---
+++ client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import asyncio
+from websockets.sync.client import connect
+
+SERVER_ADDR = "localhost"
+#SERVER_ADDR = "192.168.22.1"
+
+def hello():
+    with connect(f"ws://{SERVER_ADDR}:1337") as websocket:
+        websocket.send("Hello world!")
+        message = websocket.recv()
+        print(f"Received: {message}")
+
+hello()
