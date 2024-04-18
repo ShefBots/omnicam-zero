2a5bb43bd331515312a4854a828adc7b53827f76
Blayze Millward <blayze@blayze.tech>
2024-04-18 23:03:06 +0100

    First bit of the wizard.

--- camera-setup-wizard.py
+++ camera-setup-wizard.py
@@ -1,6 +1,6 @@
-import libcamera
 import time
-#from cameraUtils import CONFIG_IMAGES_PATH, HARDWARE_CONTROLS_FILENAME, FORMAT_CONTROLS_FILENAME
+from picamera2 import Picamera2, Preview
+from cameraUtils import ConfigType, FMT_CONFIG_FIELDS
 import cameraUtils
 import numpy as np
 import json
@@ -9,89 +9,141 @@ import argparse
 import os
 
 
-args = argparse.ArgumentParser(description="Walks the user through configuring a camera for use with the omnicam-zero sensor server. Make sure you're running this through X-forwarding SSH (ssh -X).")
-args.add_argument("-i", "--image", type=str, help="Specify a path to an image to use as the basis of the configuration wizard. If this is used, only a cropping configuration will be generated.")
+args = argparse.ArgumentParser(description="Walks the user through configuring a camera for use with the omnicam-zero sensor server. Make sure you're running this through X-forwarding SSH (ssh -X). Must be run on pi zero.")
+args.add_argument("-s", "--preview_size", type=int, nargs=2, help="The size of the preview window (used as a lores downscale during preview). Will default to maximum size if not specified")
 args = args.parse_args()
 
-if args.image not None:
-    from picamera2 import Picamera2, Preview
+if not args.preview_size:
+    print("No preview size specified.")
 
-
-# Create a place to store images if it's not there already
-cameraUtils.ensure_configuration_path()
-
-# Create backups if the file exist
-cameraUtils.backup_files_if_exist()
-
+def ask_b(s, invert=False):
+    yn_string = "Y/n" if not invert else "y/N"
+    answer = input(f"{s} [{yn_string}]: ").lower()
+    if invert and answer == "":
+        return False
+    return answer == "y" or answer == ""
 
 # Initialise the camera
 picam2 = Picamera2()
 
+# Get data
+sensor_modes = picam2.sensor_modes
+camera_controls = picam2.camera_controls
+camera_properties = picam2.camera_properties
 
-print("SENSOR MODES:")
-mode = picam2.sensor_modes[1]
-pprint(picam2.sensor_modes)
+if ask_b("Would you like to see the sensor modes, controls and properties?")
+    print("SENSOR MODES:")
+    pprint(sensor_modes)
+    print("CAMERA CONTROLS:")
+    # Note: Fully json.dumps-able
+    pprint(camera_controls)
+    print("CAMERA PROPERTIES:")
+    pprint(camera_properties) # cam_props["pixelArraySize"] indicates the maximum size
 
-print("CAMERA CONTROLS:")
-pprint(picam2.camera_controls)
-print(json.dumps(picam2.camera_controls, indent=2))
-pprint(json.loads(json.dumps(picam2.camera_controls)))
+loaded_format_config = None
+if ask_b("Would you like to attempt to load a formatting configuration file?"):
+    loaded_format_config = cameraUtils.get_config_data(ConfigType.FMT)
+    if loaded_format_config == None:
+        print("Error: No formatting config file found.")
 
-print("CAMERA PROPERTIES:")
-pprint(picam2.camera_properties) # cam_props["pixelArraySize"] indicates the maximum size
+format_config = None
+if loaded_format_config == None:
+    print("Creating default formatting config file...")
+    max_size = camera_properties["pixelArraySize"]
+    format_config = picam2.create_preview_configuration(buffer_count=4,
+                                                         queue=False,
+                                                         main={"size": max_size},#, format:"YUV420"},
+                                                         lores={"size": args.preview_size if args.preview_size else max_size},#, format:"YUV420"},
+                                                         display="lores",
+                                                         #sensor={"output_size":mode["size"], "bit_depth":mode["bit_depth"]}
+                                                        )
+else:
+    format_config = picam2.create_preview_configuration(buffer_count=format_config[FMT_CONFIG_FIELDS.BUFFER_COUNT,
+                                                        queue=format_config[FMT_CONFIG_FIELDS.QUEUE],
+                                                        main={"size": format_config[FMT_CONFIG_FIELDS.MAIN_SIZE]},#, format:"YUV420"},
+                                                        lores={"size": format_config[FMT_CONFIG_FIELDS.LORES_SIZE]},#, format:"YUV420"},
+                                                        display="lores",
+                                                        #sensor={"output_size":mode["size"], "bit_depth":mode["bit_depth"]}
+                                                       )
 
-config = cameraUtils.getDefaultFormatConfig()
-
-target_size = (240,240)
-
-# Configuring
-# Buffer count is 4 by default for preview, but more will eliminate jitter at the expense of RAM
-# queue is set to False so that we don't queue up any frames and only get the latest frame
-# lowres sets lowres
-# display sets which buffer is displayed
-config = picam2.create_preview_configuration(buffer_count=4,
-                                             queue=False,
-                                             main={"size": (1080, 1080)},#, format:"YUV420"},
-                                             lores={"size": target_size},#, format:"YUV420"},
-                                             display="lores",
-                                             #sensor={"output_size":mode["size"], "bit_depth":mode["bit_depth"]}
-                                            )
-picam2.align_configuration(config) # Auto-configure to most optimal resolution
-                                   # After this has happened, a new config["lores"/"main"] size will have been selected automagically
-print("CONFIG:")
-pprint(config)
-config["lores"]["size"] = (240,240) # Force the lores size to be 240x240 (maybe we shouldn't do this? We should probably just take a crop ourselves
-picam2.configure(config)
-#picam22.start_preview(Preview.DRM) # If we want to display a preview
+picam2.configure(format_config)
 picam2.start_preview(Preview.QT) # For transmitting over the network when connected over ssh -X
-#picam22.start_preview(Preview.NULL) # No preview (not required, but included for clarity
-
-picam2.title_fields = ["ExposureTime", "AnalogueGain"]
-
-# Notes:
-# 9.4 Manipulate camera buffers in place
-#  - Better for output?
-# 8.1 Display overlays
-# lores outputs probably *have* to be in YUV format on the zero 2
-#  - This prolly means that so does the main buffer
-# It's very much worth having two streams - the main one at a higher resolution that captures the entire image at the correct crop
-#                                         - and the lower one that is just the high-res one but down-scaled
-# We'll prolly want to request the fast sensor mode (4.2.2.3)
-# For convenience, we might want to look at using config objects (4.2. Configuration objects) instead of the create_ constructors
-#  - Probably not the best plan, since they're persistant. But the config dict is probably the best thing to change.
-# We might want to look at FrameDurationLimits to change the framerate of the camera
-
-picam2.start()
-#overlay = np.zeros((300, 400, 4), dtype=np.uint8)
-#overlay[:150, 200:] = (255, 0, 0, 64) # reddish
-#overlay[150:, :200] = (0, 255, 0, 64) # greenish
-#overlay[150:, 200:] = (0, 0, 255, 64) # blueish
-#picam2.set_overlay(overlay)
-
-
-time.sleep(2) # Let the camera stabalize
-picam2.capture_file(os.path.join(CONFIG_IMAGES_PATH,"test-image.jpg"))
-
-time.sleep(20)
+time.sleep(15)
 
 picam2.close()
+
+
+###### OLD
+#
+#
+## Initialise the camera
+#picam2 = Picamera2()
+#
+#
+#print("SENSOR MODES:")
+#mode = picam2.sensor_modes[1]
+#pprint(picam2.sensor_modes)
+#
+#print("CAMERA CONTROLS:")
+#pprint(picam2.camera_controls)
+#print(json.dumps(picam2.camera_controls, indent=2))
+#pprint(json.loads(json.dumps(picam2.camera_controls)))
+#
+#print("CAMERA PROPERTIES:")
+#pprint(picam2.camera_properties) # cam_props["pixelArraySize"] indicates the maximum size
+#
+#config = cameraUtils.getDefaultFormatConfig()
+#
+#target_size = (240,240)
+#
+## Configuring
+## Buffer count is 4 by default for preview, but more will eliminate jitter at the expense of RAM
+## queue is set to False so that we don't queue up any frames and only get the latest frame
+## lowres sets lowres
+## display sets which buffer is displayed
+#config = picam2.create_preview_configuration(buffer_count=4,
+#                                             queue=False,
+#                                             main={"size": (1080, 1080)},#, format:"YUV420"},
+#                                             lores={"size": target_size},#, format:"YUV420"},
+#                                             display="lores",
+#                                             #sensor={"output_size":mode["size"], "bit_depth":mode["bit_depth"]}
+#                                            )
+#picam2.align_configuration(config) # Auto-configure to most optimal resolution
+#                                   # After this has happened, a new config["lores"/"main"] size will have been selected automagically
+#print("CONFIG:")
+#pprint(config)
+#config["lores"]["size"] = (240,240) # Force the lores size to be 240x240 (maybe we shouldn't do this? We should probably just take a crop ourselves
+#picam2.configure(config)
+##picam22.start_preview(Preview.DRM) # If we want to display a preview
+#picam2.start_preview(Preview.QT) # For transmitting over the network when connected over ssh -X
+##picam22.start_preview(Preview.NULL) # No preview (not required, but included for clarity
+#
+#picam2.title_fields = ["ExposureTime", "AnalogueGain"]
+#
+## Notes:
+## 9.4 Manipulate camera buffers in place
+##  - Better for output?
+## 8.1 Display overlays
+## lores outputs probably *have* to be in YUV format on the zero 2
+##  - This prolly means that so does the main buffer
+## It's very much worth having two streams - the main one at a higher resolution that captures the entire image at the correct crop
+##                                         - and the lower one that is just the high-res one but down-scaled
+## We'll prolly want to request the fast sensor mode (4.2.2.3)
+## For convenience, we might want to look at using config objects (4.2. Configuration objects) instead of the create_ constructors
+##  - Probably not the best plan, since they're persistant. But the config dict is probably the best thing to change.
+## We might want to look at FrameDurationLimits to change the framerate of the camera
+#
+#picam2.start()
+##overlay = np.zeros((300, 400, 4), dtype=np.uint8)
+##overlay[:150, 200:] = (255, 0, 0, 64) # reddish
+##overlay[150:, :200] = (0, 255, 0, 64) # greenish
+##overlay[150:, 200:] = (0, 0, 255, 64) # blueish
+##picam2.set_overlay(overlay)
+#
+#
+#time.sleep(2) # Let the camera stabalize
+#picam2.capture_file(os.path.join(CONFIG_IMAGES_PATH,"test-image.jpg"))
+#
+#time.sleep(20)
+#
+#picam2.close()
--- cameraUtils.py
+++ cameraUtils.py
@@ -1,53 +1,81 @@
 import os
 import shutil
-from picamera2 import Picamera2, Preview
+from Enum import Enum
 
+class ConfigType(Enum):
+    HW = 1
+    FMT = 2
 
 CONFIG_IMAGES_PATH = "camera-configuration"
-HARDWARE_CONTROLS_FILENAME = "hardware-configuration.json"
-FORMAT_CONTROLS_FILENAME = "formatting-configuration.json"
+HARDWARE_CONTROLS_FILENAME = "hardware-configuration.json"  # Stores the physical hardware settings (wb, exposure, etc)
+FORMAT_CONTROLS_FILENAME = "formatting-configuration.json" # Stores the hardware capture size, crop and formatting configuration
+#CROP_CONFIG_FILENAME = "crop-configuration.json" # Stores the cropping and masking configuration
+
+FMT_CONFIG_FIELDS = {
+    BUFFER_COUNT:"buffer_count",
+    QUEUE:"use_queue",
+    MAIN_SIZE:"main_size",
+    LORES_SIZE:"lores_size",
+    
+
+CROP_SIZE = (240,240)
+PERFECT_CROP_IMG_FILENAME = "crop-mask.png"
 
 # Full paths
 _HW_CONTROLS_PATH = os.path.join(CONFIG_IMAGES_PATH, HARDWARE_CONTROLS_FILENAME)
 _FMT_CONTROLS_PATH = os.path.join(CONFIG_IMAGES_PATH, FORMAT_CONTROLS_FILENAME)
+#_CROP_CONFIG_PATH = os.path.join(CONFIG_IMAGES_PATH, CROP_CONFIG_FILENAME)
+_PATH_FROM_TYPE = {
+    ConfigType.HW: _HW_CONTROLS_PATH,
+    ConfigType.FMT: _FMT_CONTROLS_PATH,
+}
 
 def ensure_configuration_path():
     os.makedirs(CONFIG_IMAGES_PATH, exist_ok=True)
 
 def backup_files_if_exist():
-    if os.path.exists(_HW_CONTROLS_PATH):
-        print("Warning: Hardware control file already exists. Creating backup...")
-        shutil.copy(_HW_CONTROLS_PATH, _HW_CONTROLS_PATH + ".bk")
-    if os.path.exists(_FMT_CONTROLS_PATH):
-        print("Warning: Format control file already exists. Creating backup...")
-        shutil.copy(_FMT_CONTROLS_PATH, _FMT_CONTROLS_PATH + ".bk")
+    for p in _PATH_FROM_TYPE.values():
+        if os.path.exists(p):
+            print("Warning: Hardware control file already exists. Creating backup...")
+            shutil.copy(p, p + ".bk")
+        if os.path.exists(p):
+            print("Warning: Format control file already exists. Creating backup...")
+            shutil.copy(p, p + ".bk")
+
+def get_config_data(config_type):
+    path = _PATH_FROM_TYPE[config_type]
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            return json.load(f)
+    else:
+        return None
 
 # Applies the supplied format object to the picam2 config
 # If no format object is supplied, it loads one from disk.
 # If no format can be loaded, it gets a default.
-def apply_format_config(config, fmt_obj = None):
-    out_config = config
-    fmt = fmt_obj
-    if fmt == None:
-        if os.path.exists(_FMT_CONTROLS_PATH):
-            # Load the format file if it's found
-            with loads(open(_FMT_CONTROLS_PATH)) as loaded_fmt:
-                fmt = loaded_fmt
-        else:
-            # If no config file is found or supplied, just return the default
-            max_size = picam2.camera_properties["PixelArraySize"]
-            out_config = picam2.create_preview_configuration(buffer_count=4,
-                                                              queue=False,
-                                                              main={"size": max_size},#, format:"YUV420"},
-                                                              lores={"size": max_size},#, format:"YUV420"},
-                                                              display="main",
-                                                              #sensor={"output_size":mode["size"], "bit_depth":mode["bit_depth"]}
-                                                             )
-            return out_config
-    # Configure the config object
-    out_config["main"] = {"size": fmt["main_size"]}
-    out_config["lores"] = {"size": fmt["lores_size"]}
-    return out_config
+#def apply_format_config(config, fmt_obj = None):
+#    out_config = config
+#    fmt = fmt_obj
+#    if fmt == None:
+#        if os.path.exists(_FMT_CONTROLS_PATH):
+#            # Load the format file if it's found
+#            with loads(open(_FMT_CONTROLS_PATH)) as loaded_fmt:
+#                fmt = loaded_fmt
+#        else:
+#            # If no config file is found or supplied, just return the default
+#            max_size = picam2.camera_properties["PixelArraySize"]
+#            out_config = picam2.create_preview_configuration(buffer_count=4,
+#                                                              queue=False,
+#                                                              main={"size": max_size},#, format:"YUV420"},
+#                                                              lores={"size": max_size},#, format:"YUV420"},
+#                                                              display="main",
+#                                                              #sensor={"output_size":mode["size"], "bit_depth":mode["bit_depth"]}
+#                                                             )
+#            return out_config
+#    # Configure the config object
+#    out_config["main"] = {"size": fmt["main_size"]}
+#    out_config["lores"] = {"size": fmt["lores_size"]}
+#    return out_config
 
 
 # Saves the important format config data into the FORMAT_CONTROLS_FILE
